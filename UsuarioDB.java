9649d506e7202799253282fb81a9cc63b6240216
jean1596 <43442752+jean1596@users.noreply.github.com>
2019-06-24 23:38:19 -0500

    Update UsuarioDB.java

--- UsuarioDB.java
+++ UsuarioDB.java
@@ -13,18 +13,16 @@ import java.sql.CallableStatement;
  * @author Docente
  */
 public class UsuarioDB {
-    public static boolean AgregarUSU(String cod,String nom,String ape,String correo,String phone,String nomUsu,String contra,String direc){
+    public static boolean AgregarUSU(String nom,String ape,String correo,String phone,String contra,String direc){
     boolean rpta=false;
     try{
-        CallableStatement cl=Conexion.getConexion().prepareCall("execute A_USUARIO ?,?,?,?,?,?,?,?");
-        cl.setString(1, cod);
-        cl.setString(2, nom);
-        cl.setString(3, ape);
-        cl.setString(4, correo);
-        cl.setString(5, phone);
-        cl.setString(6, nomUsu);
-        cl.setString(7, contra);
-        cl.setString(8, direc);
+        CallableStatement cl=Conexion.getConexion().prepareCall("execute A_USUARIO ?,?,?,?,?,?");
+        cl.setString(1, nom);
+        cl.setString(2, ape);
+        cl.setString(3, correo);
+        cl.setString(4, phone);
+        cl.setString(5, contra);
+        cl.setString(6, direc);
         int i=cl.executeUpdate();
         if(i==1){rpta=true;}
         else{rpta=false;}
